791f9c9318486a57dfaf1fcce77310388813a072
andifauzy7 <andifauzy00@gmail.com>
2019-09-13 21:39:26 +0700

    Tambah Chapter 3.4 Variables

---
+++ src/Variables.java
@@ -0,0 +1,43 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Aghniya Rahma Y.
+
+
+        // Aji M. Zapar.
+
+
+        // Andi F. Dewantara.
+        int Angka = 12;
+        int Angka1 = 13, Angka2 = 14;
+
+        final int pulpen = 2500;
+        System.out.println("Beli pulpen 2 seharga " + 2*2500);
+
+        // Fahmi Widianto.
+        Angka = 12;
+        Angka1 = 13;
+        Angka2 = 14;
+        final double CM_PER_INCH = 2.54;
+        double paperWidth = 8.5;
+        double paperHeight = 11;
+        System.out.println("Paper size in centimeters: "+ paperWidth * CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+
+
+        // M. Ilham Surya.
+
+
+        // Rezky Wahyuda S.
+
+
+        // Salma Meldiyana.
+        int Vacation;
+        Vacation=5;
+        double salary;
+        salary=290;
+        System.out.println(salary);
+
+        final double CM_PER_INCHY = 2;
+
+
+    }
+}
