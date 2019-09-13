b6a573c614812ce0baa7dbda3c3c7d5fab942171
andifauzy7 <andifauzy00@gmail.com>
2019-09-13 21:33:55 +0700

    Tambah Chapter 3.3 Data Types

---
+++ src/DataTypes.java
@@ -0,0 +1,68 @@
+public class DataTypes {
+        public static void main(String[] args) {
+            // Aghniya Rahma Y.
+
+
+            // Aji M. Zapar.
+            int nilai = 500;
+            short siapa = 1000;
+            System.out.println("Anak Siapa \b");
+            System.out.println("Anak Siapa \t");
+            System.out.println(" \'Anak siapa\' ");
+            boolean ini=true;
+            if(ini == true){
+                System.out.println("Hai");
+            }else {
+                System.out.println("Hello");
+            }
+
+
+            // Andi F. Dewantara.
+                int angka = 2147483647;
+                short angka_pendek = 32767;
+                long angka_long = 3147483647l;
+                byte angka_pendekbgt = 127;
+                System.out.println(angka_long);
+
+                float desimal = 3.14E10f;
+                System.out.println(desimal);
+
+                System.out.println("\t\"Teknik\" \n\t\'Informatik\'a\b");
+
+                boolean situasi = true;
+                System.out.println(true==situasi);
+
+            // Fahmi Widianto.
+                angka = 2147483647;
+                short angka_short = 32767;
+                angka_long = 9223372036854775807l;
+                byte angka_byte = 127;
+                System.out.println(angka_short);
+
+                char karakter= 'A';
+                System.out.println("ini char" +karakter);
+
+                boolean cek=false;
+                boolean cek2=true;
+                System.out.println("test : "+cek +cek2);
+
+
+            // M. Ilham Surya.
+
+
+            // Rezky Wahyuda S.
+                // TODO code application logic here
+                int a = 5;
+                long b = 493454354;
+                byte c = 23; short d = 2;
+                float e = 3.5f;
+                System.out.println("a =" + a);
+
+                System.out.println("Carriage return \rtes");
+                System.out.println("Double Code \"tes\"");
+
+
+            // Salma Meldiyana.
+
+        }
+}
