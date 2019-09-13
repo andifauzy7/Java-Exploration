731b839346fb14e8f051abc4a3a565b3606e6177
andifauzy7 <andifauzy00@gmail.com>
2019-09-13 21:10:41 +0700

    Tambah Chapter 3.1 Simple Java Programming

---
+++ src/SimpleJava.java
@@ -0,0 +1,24 @@
+public class SimpleJava {
+    public static void main(String[] args) {
+        // Aghniya Rahma Y.
+
+
+        // Aji M. Zapar.
+
+        
+        // Andi F. Dewantara.
+        System.out.println("Happy Coding!");
+        System.out.println("Mengucapkan 'Hello World!'");
+
+        // Fahmi Widianto.
+        System.out.println("Bissmillahirahmanirrahim");
+        System.out.println("Bissmillahirahmanirrahim, 'Hello world'");
+
+        // M. Ilham Surya.
+
+        // Rezky Wahyuda S.
+
+        // Salma Meldiyana.
+
+    }
+}
