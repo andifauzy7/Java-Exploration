eeaac52ce1a48d23df2e1880987f0a96e8ccf0e6
andifauzy7 <andifauzy00@gmail.com>
2019-09-13 21:20:35 +0700

    Tambah Chapter 3.2 Comments + Perbaiki 3.1

---
+++ src/Comments.java
@@ -0,0 +1,32 @@
+public class Comments {
+    public static void main(String[] args) {
+        // Aghniya Rahma Y.
+
+
+        // Aji M. Zapar.
+
+
+        // Andi F. Dewantara.
+        System.out.println("Mengucapkan 'Hello World!'");
+        // Untuk satu baris.
+        /*
+        Untuk lebih dari 2 baris.
+        */
+
+        // Fahmi Widianto.
+        System.out.println("Bissmillahirahmanirrahim");
+        // Test tanda kutip
+       /* catatan explore tentang private class macam macam komen
+       */
+
+
+        // M. Ilham Surya.
+
+        // Rezky Wahyuda S.
+        // TODO code application logic here
+        System.out.println('B');
+
+        // Salma Meldiyana.
+
+    }
+}
--- src/SimpleJava.java
+++ src/SimpleJava.java
@@ -1,11 +1,14 @@
 public class SimpleJava {
     public static void main(String[] args) {
         // Aghniya Rahma Y.
-
+        String greeting = "Hello";
+        String s = greeting.substring(3,5);
+        System.out.println(s);
 
         // Aji M. Zapar.
+        long angka = (long) 9.22;
+        System.out.println(angka);
 
-        
         // Andi F. Dewantara.
         System.out.println("Happy Coding!");
         System.out.println("Mengucapkan 'Hello World!'");
@@ -15,10 +18,20 @@ public class SimpleJava {
         System.out.println("Bissmillahirahmanirrahim, 'Hello world'");
 
         // M. Ilham Surya.
+        int number1 =5;
+        int number2 =7;
+        System.out.println(number1+number2);
 
         // Rezky Wahyuda S.
+        float e = (float) 11.5;
+        double f = 3.53;
+        System.out.println("f =" + f);
 
         // Salma Meldiyana.
-
+        int a,b,c;
+        a=5;
+        b=3;
+        c= a+b;
+        System.out.print("hasil: "+c);
     }
 }
