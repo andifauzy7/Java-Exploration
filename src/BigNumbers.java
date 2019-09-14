a5d7c22b4ff64146f888795936348774b77e821e
andifauzy7 <andifauzy00@gmail.com>
2019-09-14 09:18:54 +0700

    Tambah Chapter 3.9 Big Numbers

---
+++ src/BigNumbers.java
@@ -0,0 +1,36 @@
+import java.math.BigInteger;
+
+public class BigNumbers {
+    public static void main(String[] args) {
+        // Aghniya Rahma Y.
+
+        // Aji M. Zapar.
+
+        // Andi F. Dewantara.
+        int nomor = 2019;
+        BigInteger angka = BigInteger.valueOf(nomor);
+        System.out.println(angka);
+
+        BigInteger angka1 = BigInteger.valueOf(2019);
+        BigInteger angka2 = BigInteger.valueOf(2001);
+        System.out.println(angka1.add(angka2));
+
+        angka1 = BigInteger.valueOf(2019);
+        angka2 = BigInteger.valueOf(2001);
+        System.out.println(angka1.subtract(angka2));
+        System.out.println(angka1.add(angka2));
+        System.out.println(angka1.multiply(angka2));
+        System.out.println(angka1.divide(angka2));
+        System.out.println(angka1.mod(angka2));
+        System.out.println(angka1.pow(2));
+
+        // Fahmi Widianto.
+
+        // M. Ilham Surya.
+
+        // Rezky Wahyuda S.
+
+        // Salma Meldiyana.
+
+    }
+}
