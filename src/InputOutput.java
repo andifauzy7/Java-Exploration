abdf7e186eabf80ec8550010b6c0593b168dd278
andifauzy7 <andifauzy00@gmail.com>
2019-09-14 09:06:38 +0700

    Tambah Chapter 3.7 Input & Output

---
+++ src/InputOutput.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        // Aghniya Rahma Y.
+
+        // Aji M. Zapar.
+
+        // Andi F. Dewantara.
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = in.nextLine();
+        // Next tidak membaca spasi.
+        System.out.println(name);
+
+        // Fahmi Widianto.
+
+        // M. Ilham Surya.
+
+        // Rezky Wahyuda S.
+
+        // Salma Meldiyana.
+
+    }
+}
